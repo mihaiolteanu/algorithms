f8ec5634cf09eb671e93f17be3e9b2249f79460e
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2016-03-22 15:44:20 +0200

    Add generic sorted array interface plus init and add implementations

---
+++ include/array_sorted.h
@@ -0,0 +1,32 @@
+/* array_sorted.h - Implementation of a resizable sorted array
+ */
+
+#ifndef ARRAY_SORTED_H
+#define ARRAY_SORTED_H
+
+#include <stddef.h>
+#include "array.h"
+
+typedef struct {
+	int (*comp)(void *, void *); // Comparison function
+	array *a;
+} array_sorted;
+
+/* Initialize a new generic resizable sorted array, where each element in the
+array has elem_size. The comparison function is used to decide the order
+of the new added elements in the array. */
+extern int array_sorted_init(array_sorted *as, size_t elem_size,
+			     int (*comp)(void *x, void *y));
+
+/* Add the element at elem_addr to the sorted array, using the compare
+function specified in the initialization. */
+extern int array_sorted_add(array_sorted *as, void *elem_addr);
+
+extern void *array_sorted_value(array_sorted *as, size_t index);
+extern void *array_sorted_search(array_sorted *as, void *elem_addr);
+extern int array_sorted_remove(array_sorted *as, size_t index);
+extern int array_sorted_size(array_sorted *as);
+extern int array_sorted_cap(array_sorted *as);
+extern void array_sorted_destroy(array_sorted *as);
+
+#endif // ARRAY_SORTED_H
---
+++ src/array_sorted.c
@@ -0,0 +1,40 @@
+#include "array_sorted.h"
+#include "array.h"
+#include "system.h"
+
+int array_sorted_init(array_sorted *as, size_t elem_size,
+		      int (*comp)(void *x, void *y)) {
+	as->comp = comp;
+	return array_init(as->a, elem_size);
+}
+
+int array_sorted_add(array_sorted *as, void *elem_addr) {
+	array *a = as->a;
+
+	if (array_size(a) == 0)
+		return array_add(a, elem_addr); // First element
+	for (int i = 0; i < array_size(a); i++) {
+		void *value = array_value(a, i);
+		if (as->comp(elem_addr, value) == -1)
+			return array_add_at_index(a, elem_addr, i);
+	}
+}
+
+void *array_sorted_value(array_sorted *as, size_t index) {
+	array *a = as->a;
+
+	return array_value(a, index);
+}
+
+void *array_sorted_search(array_sorted *as, void *elem_addr) {
+	return NULL;
+}	
+
+int array_sorted_remove(array_sorted *as, size_t index) {
+	return array_remove(as->a, index);
+}
+
+int array_sorted_size(array_sorted *as) {
+	
+}
+	
