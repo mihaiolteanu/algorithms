d8df1bcec3e8564d5dd46739d1ee6387bba2379c
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2016-04-07 12:07:52 +0300

    Update comparison function typedef comment

--- include/system.h
+++ include/system.h
@@ -1,5 +1,10 @@
+#include <stdbool.h>
+
 #define OK 1
 #define ERROR 0
 
-// Comparison function
-typedef	int (*comp_fn_t)(const void *, const void *);
+/* Comparison function type. 
+   The funtion should return an integer less than, equal to, or greater than 
+   zero if a is found, respectively, to be less than, to mach, or be greater
+   than b (same semantics as strcmp, for example) */
+typedef	int (*comp_fn_t)(const void *a, const void *b);
