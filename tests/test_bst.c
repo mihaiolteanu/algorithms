edce30f13304f6582815b6de1c5b6550b5bed70b
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2016-04-08 14:24:27 +0300

    Add bst search and insert tests

--- tests/test_bst.c
+++ tests/test_bst.c
@@ -2,12 +2,79 @@
  */
 
 #include <assert.h>
+#include <string.h>
 #include "bst.h"
+#include "common_city_struct.h"
 
-static void test_bst_search() {
-
-}
+// ***** Test functions *****
+static void test_bst_insert();
+static void test_bst_search();
 
 void run_all_bst_tests() {
+	test_bst_insert();
 	test_bst_search();
+}
+
+static void test_bst_insert() {
+	bst b;
+	bst_node *node, *left, *right;
+	city *c;
+
+	bst_init(&b, sizeof(city), comp_city_byname);
+
+	/* Insert some cities.
+	          sb
+                 /
+		cj
+	       /
+	      ab
+	*/
+	city_insert(&b, bst_insert, cities_sb_cj_ab);
+
+	// sb is the root.
+	node = bst_getroot(&b);
+	left = bst_node_left(node);
+	right = bst_node_right(node);
+	c = bst_node_value(node);
+	assert(strcmp(c->name, "sibiu") == 0);
+	// sb right node is empty
+	assert(right == NULL);
+
+	// Descend down the tree.
+	node = left;
+	left = bst_node_left(node);
+	right = bst_node_right(node);
+	c = bst_node_value(node);
+	assert(strcmp(c->name, "cluj") == 0);
+	// cj right node is empty
+	assert(right == NULL);
+
+	// Descend down the tree.
+	node = left;
+	left = bst_node_left(node);
+	right = bst_node_right(node);
+	c = bst_node_value(node);
+	assert(strcmp(c->name, "alba") == 0);
+
+	// ab has no children
+	assert(left == NULL);
+	assert(right == NULL);
+}
+
+static void test_bst_search() {
+	bst b;
+	bst_node *node, *left, *right;
+	city *c;
+
+	bst_init(&b, sizeof(city), comp_city_byname);
+
+	// Insert some cities.
+	city_insert(&b, bst_insert, cities_sb_cj_ab);
+
+	// All cities must be included in the tree.
+	for(int i = 0; i < CITIES_SB_CJ_AB_SIZE; i++)
+		city_search_byname(&b, bst_search,
+				   &cities_sb_cj_ab[i],
+				   cities_sb_cj_ab[i].size);
+
 }
