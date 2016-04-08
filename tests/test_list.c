93835ba360b0e2907c2fe440a776d443e250f04f
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2016-04-08 13:02:41 +0300

    Use the extended city in list tests

--- tests/test_list.c
+++ tests/test_list.c
@@ -11,10 +11,6 @@
 static void test_list_search();
 static void test_list_remove();
 
-// ***** Helper functions *****
-// Search city by name and assert the result with the expected size.
-static void city_search_byname(list *l, city *city_with_name, int exp_size);
-
 void run_all_list_tests() {
 	test_list_search();
 	test_list_remove();
@@ -25,22 +21,13 @@ static void test_list_search() {
 	list_init(&l, sizeof(city), comp_city_byname);
 
 	// Insert some cities.
-	// TODO: same as dict tests. Should be made into a seperate function,
-	// eventually with vargs.
-	city sb = {150, "sibiu"};
-	city cj = {300, "cluj"};
-	city ab = {200, "alba"};
-	list_add(&l, &cj);
-	list_add(&l, &ab);
-	list_add(&l, &sb);
+	city_insert(&l, list_add, cities_sb_cj_ab);
 
 	// Now let's find them by name.
-	city tosearch = {0, "sibiu"};
-	city_search_byname(&l, &tosearch, 150);
-	tosearch.name = "cluj";
-	city_search_byname(&l, &tosearch, 300);
-	tosearch.name = "alba";
-	city_search_byname(&l, &tosearch, 200);
+	for (int i = 0; i < CITIES_SB_CJ_AB_SIZE; i++)
+		city_search_byname(&l, list_search,
+				   &cities_sb_cj_ab[i],
+				   cities_sb_cj_ab[i].size);
 
 	list_destroy(&l);
 }
@@ -50,38 +37,22 @@ static void test_list_remove() {
 	list_init(&l, sizeof(city), comp_city_byname);
 
 	// Insert some cities.
-	// TODO: same as dict tests. Should be made into a seperate function,
-	// eventually with vargs.
-	city sb = {150, "sibiu"};
-	city cj = {300, "cluj"};
-	city ab = {200, "alba"};
-	list_add(&l, &cj);
-	list_add(&l, &ab);
-	list_add(&l, &sb);
+	city_insert(&l, list_add, cities_sb_cj_ab);
 
-	city toremove = {0, "alba"};
 	//Remove an element from the middle of the list.
-	list_remove(&l, &toremove);
+	city *c = &cities_sb_cj_ab[1];
+	list_remove(&l, c);
 
 	// It should not be in the list anymore.
-	city tosearch = {0, "alba"};
-	city *res = list_search(&l, &tosearch);
+	city *res = list_search(&l, c);
 	assert(res == NULL);
 
 	// But the others should.
-	tosearch.name = "cluj";
-	city_search_byname(&l, &tosearch, 300);
-	tosearch.name = "sibiu";
-	city_search_byname(&l, &tosearch, 150);
+	c = &cities_sb_cj_ab[0];
+	city_search_byname(&l, list_search, c, c->size);
+	c = &cities_sb_cj_ab[2];
+	city_search_byname(&l, list_search, c, c->size);
 
 	list_destroy(&l);
-}
-
-static void city_search_byname(list *l, city *city_with_name, int exp_size) {
-	city *res = list_search(l, city_with_name);
-	if (res != NULL)
-		assert(res->size == exp_size);
-	else
-		assert(0 && "Dictionary search returned NULL");
 }
 
