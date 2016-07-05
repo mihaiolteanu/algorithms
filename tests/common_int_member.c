7e26aaf9f9b5bf775117ce7a96f644a53334cb59
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2016-07-05 09:19:39 +0300

    [tests] Add generic function to insert ints

--- tests/common_int_member.c
+++ tests/common_int_member.c
@@ -8,3 +8,8 @@ int comp_int_member(const void *a, const void *b) {
 		return -1;
 	return avalue > bvalue;
 }
+
+void insert_ints(void *data_type, add_fn_t add_fn, int *ints, int count) {
+	for (int i = 0; i < count; i++)
+		add_fn(data_type, &ints[i]);
+}
--- tests/common_int_member.h
+++ tests/common_int_member.h
@@ -4,7 +4,11 @@
 #ifndef COMMON_INT_MEMBER
 #define COMMON_INT_MEMBER
 
+typedef void (*add_fn_t)(void *data_type, void *elem_addr);
+
 /* Comparison function for int members. */
 extern int comp_int_member(const void *a, const void *b);
+/* Insert all the ints into the data type using the add function. */
+extern void insert_ints(void *data_type, add_fn_t add_fn, int *ints, int count);
 
 #endif // COMMON_INT_MEMBER
