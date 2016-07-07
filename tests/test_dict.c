4516cb013bd43c88b4762c421b9cd0cd861010d3
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2016-07-07 11:44:43 +0300

    Add dictionary tests

--- tests/test_dict.c
+++ tests/test_dict.c
@@ -3,8 +3,10 @@
 
 #include <assert.h>
 #include <string.h>
+#define DICT_LIST
 #include "dict.h"
-#include "common_city_struct.h"
+#include "common_int_member.h"
+#include "system.h"
 
 static void test_dict_search();
 static void test_dict_max_min();
@@ -15,9 +17,26 @@ void run_all_dict_tests() {
 }
 
 static void test_dict_search() {
+	dict d;
+	int ints[] = {2, 5, 3, 10, 1};
+	int ints_size = ARRAY_SIZE(ints);
 
+	dict_init(&d, sizeof(int), comp_int_member);
+	insert_ints(&d, (add_fn_t)dict_insert, ints, ints_size);
+	assert_ints(&d, (search_fn_t)dict_search, ints, ints_size);
 }
 
 static void test_dict_max_min() {
+	dict d;
+	int ints[] = {2, 5, 3, 10, 1};
+	int ints_size = ARRAY_SIZE(ints);
 
+	dict_init(&d, sizeof(int), comp_int_member);
+	insert_ints(&d, (add_fn_t)dict_insert, ints, ints_size);
+
+	int *min = dict_min(&d);
+	int *max = dict_max(&d);
+
+	assert(*min == 1);
+	assert(*max == 10);
 }
